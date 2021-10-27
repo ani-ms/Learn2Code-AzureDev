585f1714dc9318ee80de50fc03560bf723c28919
Anirban1635163591743 <Anirban_1635163591743@tcsazuredevops.onmicrosoft.com>
2021-10-27 23:24:53 +0530

    Add project files.

---
+++ Learn2CodeStartup/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Learn2CodeStartup.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
